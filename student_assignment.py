20d74ac2d6b1a8b6bb8328cbba63274400d7b0cd
renaht_wang <RenaHT_Wang@asus.com>
2025-01-10 09:30:43 +0800

    second try

--- student_assignment.py
+++ student_assignment.py
@@ -201,12 +201,10 @@ def format_response_as_json(response):
     
  
 
-print(json.loads(generate_hw01("2024年台灣10月紀念日有哪些?")))
-#generate_hw02("列出台灣4月的紀念日")
-#generate_hw03("台灣9月的紀念日有哪些", '根據先前的節日清單，這個節日{"date": "9-1", "name": "XX紀念日"}是否有在該月份清單？')
-print(json.loads(generate_hw04("請問中華台北的積分是多少?")))
-
-
+#print(json.loads(generate_hw01("2024年台灣10月紀念日有哪些?")))
+#print(json.loads(generate_hw02("列出台灣4月的紀念日")))
+#print(json.loads(generate_hw03("台灣9月的紀念日有哪些", '根據先前的節日清單，這個節日{"date": "9-1", "name": "XX紀念日"}是否有在該月份清單？')))
+#print(json.loads(generate_hw04("請問中華台北的積分是多少?")))
 
 
 #%%  
